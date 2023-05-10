b1a3f1289ab20b2bffc9fdae14d3a528ae072105
AlexandrOAV <a0617011995@gmail.com>
2023-05-10 13:40:44 +0300

    Update task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const listEl = document.getElementById('ingredients');
+function isAddList (listArray)
+{let ingredientsItem = listArray.map(ingredient => {
+  const itemEl = document.createElement('li');
+  itemEl.textContent = ingredient;
+  itemEl.classList.add('item');
+  return itemEl;
+})
+listEl.append(...ingredientsItem);
+}
+isAddList(ingredients);
