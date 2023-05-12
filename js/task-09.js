bf22bfea3fc0065cfd281b6bb1210293e5cf5e7d
AlexandrOAV <a0617011995@gmail.com>
2023-05-12 17:44:09 +0300

    update task-09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,11 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const spanEl = document.querySelector('.color');
+const btnEl = document.querySelector('.change-color');
+const replaceColor = () =>{
+  spanEl.textContent = getRandomHexColor();
+   btnEl.style.backgroundColor = getRandomHexColor();
+}
+btnEl.addEventListener('click', replaceColor);
+
