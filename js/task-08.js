0450724c6c14e268a8a8dcace07078e96e8176cf
AlexandrOAV <a0617011995@gmail.com>
2023-05-12 16:55:27 +0300

    update task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
-
+const formEl = document.querySelector('.login-form');
+function validationForm (event) {
+event.preventDefault();
+let inputObj = {};
+const EnteredEmail = formEl.elements.email.value;
+const EnteredPassword = formEl.elements.password.value;
+if (EnteredEmail === '' || EnteredPassword === ''){
+    alert("Please enter your email and password")
+} else {
+    inputObj.email = EnteredEmail;
+    inputObj.password = EnteredPassword;
+    console.log('entered next data user', inputObj);
+    }
+};
+formEl.addEventListener('submit', validationForm)
