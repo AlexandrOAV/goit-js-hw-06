ba2f6336996b10b0466c7438cdd0dcaacf3c0cd5
AlexandrOAV <a0617011995@gmail.com>
2023-05-13 12:36:17 +0300

    update task-10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,33 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const btnCreatEl = document.querySelector('[data-create]');
+const btndestroyEl = document.querySelector('[data-destroy]');
+const boxesEl = document.getElementById('boxes');
+const inputEl = document.querySelector('#control, input');
+
+btnCreatEl.addEventListener('click', createBoxes)
+function createBoxes(amount) {
+ inputEl.addEventListener('input', () =>inputEl.value);
+  console.log(inputEl.value);
+  let boxesArray = [];
+  let width = '30px'
+  let heigth = '30px'
+  for (let i = 1; i <= inputEl.value; i++){
+    let divEl = document.createElement('div');
+     divEl.style.backgroundColor = getRandomHexColor();
+     divEl.style.margin = '5px';
+     divEl.style.width = width;
+    divEl.style.height = heigth;
+    width = Number.parseInt(width)+10 +'px';
+    heigth = Number.parseInt(heigth)+10 +'px';
+    boxesArray.push(divEl);
+  }
+  console.log(boxesArray);
+  boxesEl.append(...boxesArray);
+}
+btndestroyEl.addEventListener('click', destroyBoxes);
+function destroyBoxes() {
+  boxesEl.innerHTML = '';
+}
+
