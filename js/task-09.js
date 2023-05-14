21ebf730c702c361cb28e0d63bbec2f1affd265c
AlexandrOAV <a0617011995@gmail.com>
2023-05-14 13:13:39 +0300

    update task-09

--- js/task-09.js
+++ js/task-09.js
@@ -4,10 +4,12 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 const spanEl = document.querySelector('.color');
+const bodyEl = document.querySelector('body');
 const btnEl = document.querySelector('.change-color');
 const replaceColor = () =>{
-  spanEl.textContent = getRandomHexColor();
-   btnEl.style.backgroundColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = bodyEl.style.backgroundColor;  
 }
 btnEl.addEventListener('click', replaceColor);
+
 
