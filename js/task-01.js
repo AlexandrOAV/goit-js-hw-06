b3aec2cabb411cf0e4bb8b3bebb68dc6422006c4
AlexandrOAV <a0617011995@gmail.com>
2023-05-14 12:48:28 +0300

    update task-01

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,8 @@
-'use strict'
-
 const itemEl = document.querySelectorAll('.item');
 console.log('Number of categories:', itemEl.length);
 itemEl.forEach(item => {
-    const header = item.children;
-    console.log('Category:', header[0].textContent);
-    const lastItemEl = header[1].children.length;
+    const header = item.firstElementChild.textContent;
+    console.log('Category:', header);
+    const lastItemEl = item.lastElementChild.children.length;
     console.log('Elements:', lastItemEl);
 })
